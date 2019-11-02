4e140e229f6731a4752efaf634cff653ff2d884f
thyagofernandes <thyagofernandes>
2019-11-01 21:33:14 -0300

    Colocando os resultados dos teste em um arquivo externo

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Stress_test_mongo_index
+Este é um algoritmo em python que tem como intuíto de testar a inserção e busca do mongodb com e sem index para um exercício sugerido na disciplina de Banco de Dados NoSQL do curso de BTI da UFRN.
+
+Para executar o código use o seguinte comando:
+```python
+python Astar.py <quantidade_iterações> <quantidade_de_testes>
+```
+Ou caso exista uma versão python 2.x e outra seja 3.x:
+```python
+python3 Astar.py <quantidade_iterações> <quantidade_de_testes>
+```
+Todo resultado dos teste é colocado no arquivo tests_file.txt
--- main.py
+++ main.py
@@ -2,37 +2,55 @@ import pymongo
 import random
 import sys
 import time
+from datetime import datetime
 
 n = int(sys.argv[1])
+num_test = int(sys.argv[2])
 
 client = pymongo.MongoClient("mongodb://localhost:27017/")
 db = client["db_stress_test"]
-
 col = db["col_stress_test"]
-col.delete_many({})
-col.drop_indexes()
-start_time = time.time()
-for i in range(n):
-    col.insert_one({'val1': random.randint(0, 100),'val2':random.randint(0, 100)})
+time_test = 0
+for k in range(num_test):
+    f = open("tests_file.txt", "a")
+    now = datetime.now()
+    dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+    f.write("------------------ test performed on  " +dt_string+" with "+str(n)+ " iterations ------------------"+'\n')
+    col.delete_many({})
+    col.drop_indexes()
+    start_time = time.time()
+    for i in range(n):
+        col.insert_one({'val1': random.randint(0, 100),'val2':random.randint(0, 100)})
+    time_test = (time.time() - start_time)
+    print("--- %s insertion time in seconds" % time_test)
+    f.write("--- %s insertion time in seconds" % time_test+ '\n')
+    start_time = time.time()
+    resp =  col.find({"val1":{"$gt": -1}})
+    for i in resp:
+        pass
+    time_test = (time.time() - start_time)
+    print("--- %s search time in seconds" % time_test)
+    f.write("--- %s search time in seconds" % time_test+ '\n')
+    col.create_index([ ("val1", -1) ])
+    start_time = time.time()
+    resp =  col.find({"val1":{"$gt": -1}})
+    for i in resp:
+        pass
+    time_test = (time.time() - start_time)
+    print("--- %s search time with index in seconds" % time_test)
+    f.write("--- %s search time with index in seconds" % time_test+ '\n')
+    resp =  col.find({"val1":{"$gt": -1}} ,{"val1":1})
+    for i in resp:
+        pass
+    time_test = (time.time() - start_time)
+    print("--- %s search time with index for only val1 in seconds" % time_test)
+    f.write("--- %s search time with index for only val1 in seconds" % time_test+ '\n')
+    col.delete_many({})
+    start_time = time.time()
+    for i in range(n):
+        col.insert_one({'val1': random.randint(0, 100),'val2':random.randint(0, 100)})
+    time_test = (time.time() - start_time)
+    print("--- %s insertion time with index in seconds" % time_test)
+    f.write("--- %s insertion time with index in seconds" % time_test+ '\n'),
+f.close()
 
-print("--- %s insertion time in seconds" % (time.time() - start_time))
-start_time = time.time()
-resp =  col.find({"val1":{"$gt": -1}})
-for i in resp:
-   pass
-print("--- %s search time in seconds" % (time.time() - start_time))
-col.create_index([ ("val1", -1) ])
-start_time = time.time()
-resp =  col.find({"val1":{"$gt": -1}})
-for i in resp:
-   pass
-print("--- %s search time with index in seconds" % (time.time() - start_time))
-resp =  col.find({"val1":{"$gt": -1}} ,{"val1":1})
-for i in resp:
-    pass
-print("--- %s search time with index for only val1 in seconds" % (time.time() - start_time))
-col.delete_many({})
-start_time = time.time()
-for i in range(n):
-    col.insert_one({'val1': random.randint(0, 100),'val2':random.randint(0, 100)})
-print("--- %s insertion time with index in seconds" % (time.time() - start_time))
